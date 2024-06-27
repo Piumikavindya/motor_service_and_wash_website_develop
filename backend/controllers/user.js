294ad1a7e285f4df9dd3b7fee1717083d117919c
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-06-27 09:53:58 +0530

    create the docker images

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,7 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const validator = require('validator');
 
 const createAuthToken = (_id) => {
